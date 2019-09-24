3b461a912255cc45f7c6e49c85389c155117af0b
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-09-24 16:24:02 -0700

    Updated js to flip forwards and back

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,26 +1,15 @@
-var devs = document.getElementById("devs");
-let scrolledAmount = 0;
-let scrollByAmount = 1;
-var scroll = setInterval(
-    function () {
-        if (scrolledAmount != devs.scrollLeft) {
-            scrolledAmount = devs.scrollLeft;
-        } else if (scrolledAmount == devs.scrollLeft) {
-            scrollByAmount *= -1;
-        }
-        devs.scrollBy(scrollByAmount, 0);
-    }, 12);
 document.addEventListener('click', function (event) {
-    target = event.target;
-    devParent = target.closest(".dev");
-    if(devParent == null){
-        return;
+    cardA = event.target.closest(".cardSideA");
+    if(cardA == null){ return; }
+    if(cardA.className.includes(" flip flipBack ")){
+        cardA.className = cardA.className.replace(" flip flipBack ", "");
+        console.log("remove");
+    }else if(cardA.className.includes(" flip ")){
+        cardA.className = cardA.className.replace(" flip ", " flip flipBack ");
+        setTimeout(function(){
+            cardA.className = cardA.className.replace(" flip flipBack ", "");
+        }, .4*(1000));
+    }else{ 
+        cardA.className += " flip "; 
     }
-    if(devParent.className.includes(" flipAnimation")){
-        devParent.className = devParent.className.replace(" flipAnimation", '');
-    }else {
-        devParent.className += " flipAnimation"
-    }
-    console.log(devParent);
-
 }, false);
